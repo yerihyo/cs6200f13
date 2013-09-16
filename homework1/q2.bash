740232dafe39c5f007725f1301db3a9fee452979
Moonyoung Kang <yerihyo@gmail.com>
2013-09-16 12:18:57 -0400

    cleanup

--- homework1/q2.bash
+++ homework1/q2.bash
@@ -3,4 +3,7 @@
 set -e
 set -u
 
-wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' -O /tmp/t http://www.ccs.neu.edu
+agent_string='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4'
+wget -U "$agent_string" --recursive --accept=html,pdf --no-directories --wait=5 http://www.ccs.neu.edu
+
+#wget --no-parent --wait=10 --limit-rate=100K --recursive --accept=jpg,jpeg --no-directories http://somedomain/images/page1.html
